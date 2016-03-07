ae6d0ddd6562bb5042785ad0a9a790af378f754a
hypergenesis <riverhunt1022@yahoo.com>
2016-03-07 22:34:23 +0000

    Lab10

--- topic2/lab10/temperature.h
+++ topic2/lab10/temperature.h
@@ -1,5 +1,5 @@
 /*
- * Name        : temperature.h
+ * Name        : Temperature.h
  * Author      : FILL IN
  * Description : Class Header File
  */
@@ -14,7 +14,7 @@ using std::stringstream;
 
 /*
  * Class Temperature.
- * A class that converts temperatures. It will always internally store the value
+ * A class that converts Temperatures. It will always internally store the value
  * in kelvin.
  */
 class Temperature {
@@ -34,10 +34,10 @@ class Temperature {
 
   /*
    * Constructor #3.
-   * Converts the supplied temperature to kelvin and internally stores it.
-   * The temperature's unit will be provided in the second argument.
+   * Converts the supplied Temperature to kelvin and internally stores it.
+   * The Temperature's unit will be provided in the second argument.
    * If the second argument is not value (i.e. not 'F' or 'C') assume the
-   * temperature is in kelvin
+   * Temperature is in kelvin
    * @param double temp - The value to set the internal kelvin to once
    *                    - converted.
    * @param char unit - The type of unit temp is. Will be either 'F' or 'C',
@@ -46,7 +46,7 @@ class Temperature {
   Temperature(double temp, char unit);
 
   /*
-   * The temperature will come in as kelvin and this function will set the
+   * The Temperature will come in as kelvin and this function will set the
    * internal temp to this value
    * @param double kelvin - The value to set the internal kelvin to.
    */
@@ -54,7 +54,7 @@ class Temperature {
 
 
   /*
-   * The temperature will come in as Celsius and this function will set the
+   * The Temperature will come in as Celsius and this function will set the
    * internal temp to this value, once converted to kelvin
    * Formula: k = c + 273.15
    * @param double celsius - The value (in celsius) to set the internal kelvin
@@ -64,7 +64,7 @@ class Temperature {
 
 
   /*
-   * The temperature will come in as Fahrenheit and this function will set the
+   * The Temperature will come in as Fahrenheit and this function will set the
    * internal temp to this value, once converted to kelvin
    * Formula: k = (5.0 * (f - 32) / 9) + 273.15
    * @param double fahrenheit - The value (in fahrenheit) to set the internal
@@ -73,30 +73,30 @@ class Temperature {
   void SetTempFromFahrenheit(double fahrenheit);
 
   /*
-   * Gets the internal temperature in Kelvin.
-   * @return double - The temperature in Kelvin
+   * Gets the internal Temperature in Kelvin.
+   * @return double - The Temperature in Kelvin
    */
   double GetTempAsKelvin() const;
 
   /*
    * Returns the internal temp converted to Celsius
    * Formula: k - 273.15
-   * @return double - The temperature in Celsius
+   * @return double - The Temperature in Celsius
    */
   double GetTempAsCelsius() const;
 
   /*
    * Returns the internal temp converted to Fahrenheit
    * Formula: ((c * 9.0) / 5) + 32;
-   * @return double - The temperature in Fahrenheit
+   * @return double - The Temperature in Fahrenheit
    */
   double GetTempAsFahrenheit() const;
 
   /*
-   * Get a string representation of the temperature.
+   * Get a string representation of the Temperature.
    * The string will be formatted as:
    * "TEMP UNIT"
-   * where TEMP is the temperature to 2 decimal places and UNIT is either
+   * where TEMP is the Temperature to 2 decimal places and UNIT is either
    * "Kelvin", "Celsius", or "Fahrenheit".
    * The conversion to perform is denoted by the parameter.
    * If the unit given through the argument is invalid, set the string to:
@@ -104,7 +104,7 @@ class Temperature {
    * @uses stringstream
    * @param char unit - The conversion to perform, either 'K', 'C' or 'F',
    *                    defaults to 'K' and is case-insensitive
-   * @return string - A string representation of the temperature or invalid if
+   * @return string - A string representation of the Temperature or invalid if
    *                  the provided unit is not recognized
    */
   string ToString(char unit = 'K') const;
