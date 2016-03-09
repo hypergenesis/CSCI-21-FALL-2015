d8d7b4dce5221dd9237db77787382dc712c76b1c
hypergenesis <riverhunt1022@yahoo.com>
2016-03-09 23:17:44 +0000

    lab10

--- topic2/lab10/temperature.h
+++ topic2/lab10/temperature.h
@@ -1,11 +1,12 @@
 /*
  * Name        : Temperature.h
- * Author      : FILL IN
+ * Author      : River Roseveare-Hunt
  * Description : Class Header File
  */
 
 // ADD HEADER GUARD HERE
-
+#ifndef LAB_H
+#define LAB_H
 
 #include <string>
 #include <sstream>
@@ -114,3 +115,4 @@ class Temperature {
 };
 
 // REST OF HEADER GUARD GOES HERE
+#endif
